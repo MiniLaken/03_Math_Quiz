485e875e5e0922600d33514fad6501dcdcdc1d62
MiniLaken <tanielul3741@masseyhigh.school.nz>
2025-05-12 11:05:33 +1200

    half working

--- 03_HL_Instructions.py
+++ 03_HL_Instructions.py
@@ -14,16 +14,4 @@ def yes_no(question):
             print("please enter yes / no")
 
 def instruction():
-    print()
-
-# Instructions
-
-# to begin, decide on a score goal (eg: The first one to get a
-#  score of 50 wins).
-
-# For each round of the game, you win points by rolling dice.
-# The winner of the round is the one who gets 13 (or slightly less).
-
-# If you win the round, then your score will increase by the 
-# number of points that you earned. If your first roll of two dice is a double (eg: both dice show a three), then your score will be DOUBLE the 
-# number of points.
+    print("Choose how many rounds you want.")
